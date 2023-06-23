190eef460f68a4a13f1c25aab8f384003f111cc6
WuX <axel.riska@gmail.com>
2023-06-23 23:19:25 +0200

    Mapping entries past 4gb + Fixed typo with uefi handoff

--- core/src/memory/vmm.amd64.c
+++ core/src/memory/vmm.amd64.c
@@ -57,6 +57,22 @@ vmm_address_space_t vmm_initialize() {
     vmm_map(map, PAGE_SIZE, PAGE_SIZE, FOUR_GB - PAGE_SIZE);
     vmm_map(map, PAGE_SIZE, HHDM_OFFSET + PAGE_SIZE, FOUR_GB - PAGE_SIZE);
 
-    // TODO: Map memory map entries past 4GB
+    for(int i = 0; i < g_pmm_map_size; i++) {
+        if(g_pmm_map[i].base + g_pmm_map[i].length < FOUR_GB) continue;
+        uint64_t base = g_pmm_map[i].base;
+        uint64_t length = g_pmm_map[i].length;
+        if(base < FOUR_GB) {
+            length -= FOUR_GB - base;
+            base = FOUR_GB;
+        }
+        if(base % PAGE_SIZE != 0) {
+            length += base % PAGE_SIZE;
+            base -= base % PAGE_SIZE;
+        }
+        if(length % PAGE_SIZE != 0) {
+            length += PAGE_SIZE - length % PAGE_SIZE;
+        }
+        vmm_map(map, base, base, length);
+    }
     return map;
 }
