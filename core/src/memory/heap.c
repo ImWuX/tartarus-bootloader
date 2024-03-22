0342541b85e22aebfae48af00d36660add0e7a7a
WuX <axel.riska@gmail.com>
2024-03-22 01:05:15 +0100

    heap: added realloc

--- core/src/memory/heap.c
+++ core/src/memory/heap.c
@@ -1,4 +1,5 @@
 #include "heap.h"
+#include <lib/mem.h>
 #include <lib/math.h>
 #include <log.h>
 #include <memory/pmm.h>
@@ -50,6 +51,15 @@ void *heap_alloc(size_t size) {
     }
     alloc(MATH_DIV_CEIL(size + sizeof(heap_entry_t), PMM_PAGE_SIZE));
     return heap_alloc(size);
+}
+
+void *heap_realloc(void *ptr, size_t new_size) {
+    void *new_ptr = heap_alloc(new_size);
+    if(ptr == NULL) return new_ptr;
+    heap_entry_t *entry = (heap_entry_t *) ((uintptr_t) ptr - sizeof(heap_entry_t));
+    memcpy(new_ptr, ptr, new_size > entry->size ? entry->size : new_size);
+    heap_free(ptr);
+    return new_ptr;
 }
 
 void heap_free(void *address) {
--- core/src/memory/heap.h
+++ core/src/memory/heap.h
@@ -3,4 +3,5 @@
 #include <stddef.h>
 
 void *heap_alloc(size_t size);
+void *heap_realloc(void *ptr, size_t new_size);
 void heap_free(void *address);
